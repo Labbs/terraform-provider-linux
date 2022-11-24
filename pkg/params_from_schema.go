483fa65e2796e5f76622c30570ee2871f7eb7689
Labbs <moutonjeremy@labbs.fr>
2022-11-24 13:07:06 +0100

    change path

--- pkg/params_from_schema.go
+++ pkg/params_from_schema.go
@@ -2,11 +2,12 @@ package pkg
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/labbs/terraform-provider-linux/pkg/client"
 	"github.com/spf13/cast"
 )
 
-func paramsFromSchema(d *schema.ResourceData) *conn {
-	return &conn{
+func paramsFromSchema(d *schema.ResourceData) *client.Conn {
+	return &client.Conn{
 		Host:       cast.ToString(d.Get(attrProviderHost)),
 		Port:       cast.ToInt(d.Get(attrProviderPort)),
 		User:       cast.ToString(d.Get(attrProviderUser)),
